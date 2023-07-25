617a1e0b9babdea3e8237feaae94e2aebbec172a
rash <orashusedmund@gmail.com>
2023-07-25 14:02:15 +0100

    modified index.tsx

--- src/components/views/details/desktop/index.tsx
+++ src/components/views/details/desktop/index.tsx
@@ -1,10 +1,7 @@
-import Button from "@/components/atoms/button";
-
 const DetailsDesktop = () => {
   return (
     <main>
       <h1>Details Desktop</h1>
-      <Button type="button" text="Test button" btn_type="outlined" light_gray sx={{ width: "100px" }} />
     </main>
   );
 };
